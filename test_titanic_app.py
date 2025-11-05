b580311eec6efaa34aa439625b3aad451dff7a8b
icemanpavel-bvb <iceman.pavel@gmail.com>
2025-11-05 19:11:08 +0500

    проба запуска автоматической проверки, добавил комментарии в файлы с кодом

--- test_titanic_app.py
+++ test_titanic_app.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas as pd				#загрузка библиотек
 import numpy as np
 import pytest
 
--- titanik_app.py
+++ titanik_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as st				#Загрузка бибилотек
 import pandas as pd
 import numpy as np
 
